fe5cf5dffd6f92b4d64deb0152dc5267b0ab4589
jamie <jamieapps101@gmail.com>
2021-12-07 23:12:02 +0000

    Day one - part two

--- main.py
+++ main.py
@@ -19,23 +19,34 @@ def get_deltas(input_data):
     decrease_counter = 0
     equal_counter = 0
     for i in range(len(input_data)):
-        if i == 0:
-            deltas.append("N/A")
+        if i < 2:
             continue
+        elif i == 2:
+            deltas.append("N/A")
         else:
-            if input_data[i-1] < input_data[i]:
+            # start on the 4th element, but look back at
+            # the first 3
+            first_triple  = input_data[i-3:i]
+            second_triple = input_data[i-2:i+1]
+
+            first_sum = sum(first_triple)
+            second_sum = sum(second_triple)
+
+            if first_sum < second_sum:
                 deltas.append("increase")
                 increase_counter += 1
-            elif input_data[i-1] > input_data[i]:
+            elif first_sum > second_sum:
                 deltas.append("decrease")
                 decrease_counter += 1
             else:
                 deltas.append("equal")
                 equal_counter += 1
     
+
     print("increase_counter: {}".format(increase_counter))
     print("decrease_counter: {}".format(decrease_counter))
     print(f"equal_counter: {equal_counter}")
+    return deltas
 
 
 
@@ -43,7 +54,9 @@ def get_deltas(input_data):
 def main():
     print("Starting")
     input_data = get_input_data(FILE_PATH)
-    get_deltas(input_data)
+    deltas = get_deltas(input_data)
+    print(deltas[0:8])
+
 
 
 
