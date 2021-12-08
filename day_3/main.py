7b1e7de04a1624fa84b96000633d594f9d22537f
justletmeliv <oabwebster@gmail.com>
2021-12-08 23:44:35 +0000

    Day three - part one data import
    Got the import_diagnostic_report function working and cleansed input data. Works fine!

--- day_3/main.py
+++ day_3/main.py
@@ -1,7 +1,15 @@
+FILE_PATH = "./day_3/data/input.txt"
+
+def import_diagnostic_report(file_path: str):
+    input_data = []
+    with open(file_path) as fp:
+        for line in fp:
+            input_data.append(line[:-1])
+    return input_data
+
 def main():
-    pass
-
-
+    input_data = import_diagnostic_report(FILE_PATH)
+    print(input_data)
 
 if __name__=="__main__":
     main()
