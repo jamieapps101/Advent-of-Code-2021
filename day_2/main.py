ddbb660458fb96c0099961af7ccc437c84d8a374
jamie <jamieapps101@gmail.com>
2021-12-08 00:29:40 +0000

    Day two - part one

---
+++ day_2/main.py
@@ -0,0 +1,53 @@
+from typing import List
+
+
+FILE_PATH = "./day_2/data/input.txt"
+
+
+def import_instructions(file_path: str):
+    instructions = []
+    # fp = open(file_path)
+    with open(file_path) as fp:
+        for line in fp:
+            # This reads in each line in the file
+            # and puts it onto the end of the 
+            # instructions list
+            # instructions.append(line)
+            if line == "":
+                break
+            parts = line.split()
+            keyword = parts[0]
+            value = int(parts[1])
+            instructions.append({
+                "keyword": keyword,
+                "value": value
+            })
+    return instructions
+
+
+def act_on_instructions(instructions: List[dict]):
+    horizontal_position = 0
+    depth = 0
+    for instruction in instructions:
+        if instruction["keyword"] == "forward":
+            horizontal_position += instruction["value"]
+        elif instruction["keyword"] == "up":
+            depth -= instruction["value"]
+        elif instruction["keyword"] == "down":
+            depth += instruction["value"]
+        else:
+            print("ERRRRRRRRRRRor")
+            raise Exception
+    final_position = horizontal_position * depth
+    print(f"final_position: {final_position}")
+
+
+
+def main():
+    instructions = import_instructions(FILE_PATH)   
+    act_on_instructions(instructions) 
+
+
+
+if __name__ == "__main__":
+    main()
