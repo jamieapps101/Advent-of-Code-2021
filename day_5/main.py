287e19ca0305a4d47523ef7ef71b599ef4591403
jamie <jamieapp101@gmail.com>
2022-01-04 00:18:40 +0000

    working

--- day_5/main.py
+++ day_5/main.py
@@ -6,7 +6,7 @@ from typing import List
 FILE_PATH = "./day_5/data/input.txt"
 
 class Point:
-    def __init__(self,x,y):
+    def __init__(self,x:int,y:int):
         self.x = x
         self.y = y
 
@@ -22,6 +22,9 @@ class Point:
             self.y + other.y,
         )
 
+    def __format__(self, __format_spec: str) -> str:
+        return f"({self.x},{self.y})"
+
 class Vector:
     def __init__(self,p0:Point,p1:Point):
         self.p0 = p0
@@ -35,16 +38,65 @@ class Vector:
         return v
 
     def is_horizontal(self) -> bool:
-        self.p0.y == self.p1.y
+        return self.p0.y == self.p1.y
 
     def is_vertical(self) -> bool:
-        self.p0.x == self.p1.x
+        return self.p0.x == self.p1.x
 
     def get_points(self) -> List[Point]:
-        pass
+        points = []
+        if self.is_horizontal():
+            start = self.p0.x
+            stop = self.p1.x
+            step = 1 if start<=stop else -1
+            stop += step
+            for x in range(start,stop,step):
+                points.append(Point(x,self.p0.y))
+        elif self.is_vertical():
+            start = self.p0.y
+            stop = self.p1.y
+            step = 1 if start<=stop else -1
+            stop += step
+            for y in range(start,stop,step):
+                points.append(Point(self.p0.x,y))
+        else:
+            # aka this is a diagonal
+            m = (self.p1.y-self.p0.y)/(self.p1.x-self.p0.x)
+            c = self.p0.y - m*self.p0.x
+            raise NotImplemented
+        return points
+
+    def __format__(self, __format_spec: str) -> str:
+        return f"{self.p0}->{self.p1}"
+
+class Map:
+    def __init__(self, max_size: Point):
+        self.size = max_size
+        self.data = [ [0 for _row_index in range(max_size.y+1)] for _col_index in range(max_size.x+1) ]
+
+    def add_to_point(self, p: Point):
+        self.data[p.x][p.y] += 1
+
+    def get_points_with_count_above(self, thresh: int) -> int:
+        point_count = 0
+        for x in range(self.size.x):
+            for y in range(self.size.y):
+                if self.data[x][y]>thresh:
+                    point_count += 1
+        return point_count
 
 
-def read_data(file_path: str):
+def get_max_point(vectors: List[Vector]) -> Point:
+    max_point = Point(0,0)
+    for v in vectors:
+        for p in [v.p0,v.p1]:
+            if p.x > max_point.x:
+                max_point.x = p.x
+            if p.y > max_point.y:
+                max_point.y = p.y
+    return max_point
+
+def read_data(file_path: str) -> List[Vector]:
     input_vectors = []
     with open(file_path) as fp:
         for line in fp:
@@ -64,7 +116,18 @@ def read_data(file_path: str):
 
 
 def main():
-    read_data(FILE_PATH)
+    vectors = read_data(FILE_PATH)
+    filtered_vectors = [v for v in vectors if v.is_vertical() or v.is_horizontal()]
+    map_size = get_max_point(filtered_vectors)
+    print(f"map_size: {map_size}")
+    map = Map(map_size)
+    for v in filtered_vectors:
+        v_points = v.get_points()
+        for p in v_points:
+            map.add_to_point(p)
+    point_count = map.get_points_with_count_above(1)
+    print(f"point_count: {point_count}")
+
 
 if __name__=="__main__":
     main()
