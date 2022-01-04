d0f7c7c4bc5aa5c39da38601799a53ca72ed182f
justletmeliv <oabwebster@gmail.com>
2022-01-04 21:37:10 +0000

    get_data Function Working

--- day_6/main.py
+++ day_6/main.py
@@ -1,7 +1,17 @@
 #! /usr/bin/env python3
 
+FILE_PATH = "day_6/data/input.txt"
+
+def get_data(file_path: str):
+    input_data = []
+    with open(file_path) as fp:
+        for line in fp:
+            input_data.extend([int(n) for n in line.split(",")])
+    return input_data
+
 def main():
-    pass
+    data = get_data(FILE_PATH)
+    print(f"test data = {data}")
 
 if __name__=="__main__":
     main()
