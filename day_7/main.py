bf767b3283fd4f78cbbcf09e19dd85cbf676e14c
jamie <jamieapp101@gmail.com>
2022-01-05 22:19:13 +0000

    solution method using binary search algorithm

--- day_7/main.py
+++ day_7/main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from typing import List
+from typing import List,Tuple
 
 FILE_PATH = "day_7/data/input.txt"
 
@@ -26,10 +26,32 @@ def fuel_cost(current_positions: List[int], proposed_position: int):
             absolute_difference += position - proposed_position
         else:
             absolute_difference += proposed_position - position
-    print(f"Absolute Difference: {absolute_difference}")
     return absolute_difference
     # get absolute difference between 'proposed' position and actual position of each item in the list
     # sum absolute differences up
+
+def mean(a,b):
+    return (a+b)/2
+
+def get_solution(data: List[int], precision: int = 10) -> Tuple[int,int]:
+    a = max(data)
+    c = min(data)
+    b = mean(a,c)
+    for i in range(precision):
+        print(f"a = {a}, c = {c}")
+        candidate_a = mean(a,b)
+        candidate_a_cost = fuel_cost(data,candidate_a)
+        candidate_b = mean(b,c)
+        candidate_b_cost = fuel_cost(data,candidate_b)
+        if candidate_a_cost <= candidate_b_cost:
+            a = a
+            c = b
+        else:
+            a = b
+            c = c
+        b = mean(a,c)
+        print(f"cost: {fuel_cost(data,b)}\n")
+    return (b,fuel_cost(data,b))
 
 def main():
     data = get_data(FILE_PATH)
@@ -38,6 +60,8 @@ def main():
     print(f"Maximum Current Position: {maximum_current_position}")
     minimum_current_position = min_current_position(data)
     print(f"Minimum Current Position: {minimum_current_position}")
+    solution = get_solution(data,precision=20)
+    print(f"solution is {int(solution[0])} with cost {int(solution[1])}")
 
 if __name__=="__main__":
     main()
