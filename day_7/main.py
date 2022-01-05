cc70de7b405d78e2a497b27e3a947d85d6dc1e4a
jamie <jamieapp101@gmail.com>
2022-01-05 22:43:12 +0000

    updated fuel cost function

--- day_7/main.py
+++ day_7/main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from typing import List
+from typing import List,Tuple
 
 FILE_PATH = "day_7/data/input.txt"
 
@@ -19,46 +19,40 @@ def max_current_position(positions: List[int]):
 def min_current_position(positions: List[int]):
     return min(positions)
 
+def get_triangle_number(n:int) -> int:
+    return sum([x+1 for x in range(int(n))])
+
 def fuel_cost(current_positions: List[int], proposed_position: int):
-    absolute_difference = 0
-    for position in current_positions:
-        if position >= proposed_position:
-            absolute_difference += position - proposed_position
-        else:
-            absolute_difference += proposed_position - position
-    print(f"Absolute Difference: {absolute_difference}")
-    return absolute_difference
     # get absolute difference between 'proposed' position and actual position of each item in the list
     # sum absolute differences up
+    cost = 0
+    for position in current_positions:
+        cost += get_triangle_number(abs(position - proposed_position))
+    return cost
 
-def get_solution(data: List[int],min_current_position: int, max_current_position: int):
-    lowest_cost = 0
-    best_position = 0
-    for position in range(min_current_position,max_current_position + 1):
-        position_cost = fuel_cost(data,position)
-        if position == min_current_position:
-            lowest_cost = position_cost
-            best_position = position
-            print(f"Lowest Cost: {lowest_cost}")
-            print (f"Best Position: {best_position}")
-        elif position_cost < lowest_cost:
-            lowest_cost = position_cost
-            best_position = position
-    return lowest_cost,best_position
-# max/min positions
-# iterate over values in that range for possible solutions
-# for each possible solution, call fuel_cost
-# identify the lowest number and return it
+def mean(a,b):
+    return (a+b)/2
+
+def get_solution(data: List[int], precision: int = 10) -> Tuple[int,int]:
+    a = max(data)
+    c = min(data)
+    b = mean(a,c)
+    for i in range(precision):
+        print(f"{i}) a={a}, c={c} - ",end="")
+        if fuel_cost(data,int(mean(a,b))) <= fuel_cost(data,int(mean(b,c))):
+            # a = a
+            c = b
+        else:
+            a = b
+            # c = c
+        b = mean(a,c)
+        print(f"cost={fuel_cost(data,b)}")
+    return (b,fuel_cost(data,b))
 
 def main():
     data = get_data(FILE_PATH)
-    maximum_current_position = max_current_position(data)
-    print(f"Maximum Current Position: {maximum_current_position}")
-    minimum_current_position = min_current_position(data)
-    print(f"Minimum Current Position: {minimum_current_position}")
-    potential_solution = get_solution(data,minimum_current_position,maximum_current_position)
-    print(f"Allegedly Lowest Cost: {potential_solution[0]}")
-    print(f"Allegedly Best Solution: {potential_solution[1]}")
+    solution = get_solution(data,precision=10)
+    print(f"solution is {int(solution[0])} with cost {int(solution[1])}")
 
 if __name__=="__main__":
     main()
