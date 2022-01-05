413a3acd7849d943852fca863b5984d0fe8dfb0e
justletmeliv <oabwebster@gmail.com>
2022-01-05 22:32:30 +0000

    It Works!

--- day_7/main.py
+++ day_7/main.py
@@ -31,13 +31,34 @@ def fuel_cost(current_positions: List[int], proposed_position: int):
     # get absolute difference between 'proposed' position and actual position of each item in the list
     # sum absolute differences up
 
+def get_solution(data: List[int],min_current_position: int, max_current_position: int):
+    lowest_cost = 0
+    best_position = 0
+    for position in range(min_current_position,max_current_position + 1):
+        position_cost = fuel_cost(data,position)
+        if position == min_current_position:
+            lowest_cost = position_cost
+            best_position = position
+            print(f"Lowest Cost: {lowest_cost}")
+            print (f"Best Position: {best_position}")
+        elif position_cost < lowest_cost:
+            lowest_cost = position_cost
+            best_position = position
+    return lowest_cost,best_position
+# max/min positions
+# iterate over values in that range for possible solutions
+# for each possible solution, call fuel_cost
+# identify the lowest number and return it
+
 def main():
     data = get_data(FILE_PATH)
-    fuel_cost(data,750)
     maximum_current_position = max_current_position(data)
     print(f"Maximum Current Position: {maximum_current_position}")
     minimum_current_position = min_current_position(data)
     print(f"Minimum Current Position: {minimum_current_position}")
+    potential_solution = get_solution(data,minimum_current_position,maximum_current_position)
+    print(f"Allegedly Lowest Cost: {potential_solution[0]}")
+    print(f"Allegedly Best Solution: {potential_solution[1]}")
 
 if __name__=="__main__":
     main()
