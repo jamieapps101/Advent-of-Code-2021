217e656577c41f7681ba62b25befe5fa9b456382
justletmeliv <oabwebster@gmail.com>
2022-01-08 20:51:58 +0000

    Data Hopefully Reading In Correctly?

--- day_9/main.py
+++ day_9/main.py
@@ -3,20 +3,19 @@
 FILE_PATH = "day_9/data/input.txt"
 
 def get_data(file_path: str):
-    digit_data = []
     line_data = []
     with open(file_path) as fp:
         for line in fp:
             line = line.rstrip()
+            digit_data = []
             for digit in line:
                 number = int(digit)
                 digit_data.append(number)
-            # line_data.append(line)
+            line_data.append(digit_data)
     # print(f"Digit Data = {digit_data}")
     # print(f"Line Data = {line_data}")
-    line_data.append(digit_data)
     print(f"Input Data = {line_data}")
-    return line_data, digit_data
+    return line_data
 
 def main():
     data = get_data(FILE_PATH)
