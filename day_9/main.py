c930ad2424710e9cd92f83748ca7277049076f90
justletmeliv <oabwebster@gmail.com>
2022-01-09 00:03:02 +0000

    Broken Logic for Risk Level?

--- day_9/main.py
+++ day_9/main.py
@@ -32,28 +32,29 @@ def access_element(line_data: List):
     print(f"Length of X Dimension: {max_x_dimension}")
     print(f"Length of Y Dimension: {max_y_dimension}")
     # edge_case_count = 0
+    risk_level = 0
     for row_index in range(max_y_dimension):
         for column_index in range(max_x_dimension):
             element = line_data[row_index][column_index]
             # print(f"Element: {element}")
+            if row_index == 0 or row_index == max_x_dimension-1 or column_index == 0 or column_index == max_y_dimension-1:
+                continue
             look_above = line_data[row_index-1][column_index]
             look_below = line_data[row_index+1][column_index]
             look_left = line_data[row_index][column_index-1]
             look_right = line_data[row_index][column_index+1]
             # print(f"Column Index: {column_index}")
             # print(f"Row Index: {row_index}")
-            print(f"Above: {look_above}")
-            print(f"Below: {look_below}")
-            print(f"Left: {look_left}")
-            print(f"Right: {look_right}")
+            # print(f"Above: {look_above}")
+            # print(f"Below: {look_below}")
+            # print(f"Left: {look_left}")
+            # print(f"Right: {look_right}")
+            if look_above < element and look_below < element and look_left < element and look_right < element:
+                risk_level += element + 1
+                print(f"Risk Level = {risk_level}")
+            
 
-            if row_index == 0 or row_index == max_x_dimension-1 or column_index == 0 or column_index == max_y_dimension-1:
-                continue
-            return
 
-             
-                # edge_case_count += 1
-            # print(f"Edge Case Count: {edge_case_count}")
     
 # 5. Within nested for loops, we will need two sections of code: first, detect if we are at an edge case (if statement) and will skip this iteration of the for loops if so. Second, will detect the local minimum value.
 # 6. Inside the second bit, that is what we are using to detect if the current central value is minimum to everything else.
