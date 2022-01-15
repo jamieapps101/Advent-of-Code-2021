2a50f91c4afd16390879b2fefec825b689008e8f
jamie <jamieapp101@gmail.com>
2022-01-15 23:50:25 +0000

    Working
    Used graph exploration algorithm to group traversable points

--- day_9/main.py
+++ day_9/main.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
 
+from pprint import pprint
 from typing import List
 
-FILE_PATH = "day_9/data/test_input.txt"
-# FILE_PATH = "day_9/data/input.txt"
+# FILE_PATH = "day_9/data/test_input.txt"
+FILE_PATH = "day_9/data/input.txt"
 
 def get_data(file_path: str):
     line_data = []
@@ -17,115 +18,6 @@ def get_data(file_path: str):
             line_data.append(digit_data)
     return line_data
 
-def basin_labeller(line_data: List):
-    pass
-# ----------- Day 9 Part 1 Code -----------------------------------------------------
-def check_element(line_data: List):
-    max_x_dimension = (len(line_data[0]))
-    max_y_dimension = (len(line_data))
-    # print(f"Length of X Dimension: {max_x_dimension}")
-    # print(f"Length of Y Dimension: {max_y_dimension}")
-    # edge_case_count = 0
-    risk_level = 0
-    for row_index in range(max_y_dimension):
-        for column_index in range(max_x_dimension):
-            element = line_data[row_index][column_index]
-            # print(f"Element: {element}")
-            # if row_index == 0 or row_index == max_x_dimension-1 or column_index == 0 or column_index == max_y_dimension-1:
-            #     continue
-
-            # look_below = line_data[row_index+1][column_index]
-            # look_above = line_data[row_index-1][column_index]
-            # look_left = line_data[row_index][column_index-1]
-            # look_right = line_data[row_index][column_index+1]
-
-            # print(f"Column Index: {column_index}")
-            # print(f"Row Index: {row_index}")
-            # print(f"Above: {look_above}")
-            # print(f"Below: {look_below}")
-            # print(f"Left: {look_left}")
-            # print(f"Right: {look_right}")
-            # if look_above > element and look_below > element and look_left > element and look_right > element:
-            #     risk_level += element + 1
-            # edges
-
-            if row_index == 0 and (column_index != 0 and column_index != max_y_dimension-1):
-            # if row_index == 0 and (column_index != 0 or column_index != max_y_dimension-1):
-            # if row_index == 0 and not (column_index == 0 or column_index == max_y_dimension-1):
-                look_below = line_data[row_index+1][column_index]
-                look_left = line_data[row_index][column_index-1]
-                look_right = line_data[row_index][column_index+1]
-                if look_below > element and look_left > element and look_right > element:
-                    risk_level += element + 1
-                    print("Top Line Edge Case Found")
-
-            elif row_index == max_x_dimension-1 and (column_index != 0 and column_index != max_y_dimension-1):
-                look_above = line_data[row_index-1][column_index]
-                look_left = line_data[row_index][column_index-1]
-                look_right = line_data[row_index][column_index+1]
-                if look_above > element and look_left > element and look_right > element:
-                    risk_level += element + 1
-                    print("Bottom Line Edge Case Found")
-
-            elif column_index == 0 and (row_index != 0 and row_index != max_x_dimension-1):
-                look_below = line_data[row_index+1][column_index]
-                look_above = line_data[row_index-1][column_index]
-                look_right = line_data[row_index][column_index+1]
-                if look_above > element and look_below > element and look_right > element:
-                    risk_level += element + 1
-                    print("Left Side Edge Case Found")
-            elif column_index == max_y_dimension-1 and (row_index != 0 and row_index != max_x_dimension-1):
-                look_below = line_data[row_index+1][column_index]
-                look_above = line_data[row_index-1][column_index]
-                look_left = line_data[row_index][column_index-1]
-                if look_above > element and look_below > element and look_left > element:
-                    risk_level += element + 1
-                    print("Right Side Edge Case Found")
-            # corners
-            elif row_index == 0 and column_index == 0:
-                look_below = line_data[row_index+1][column_index]
-                look_right = line_data[row_index][column_index+1]
-                if look_below > element and look_right > element:
-                    risk_level += element + 1
-            elif row_index == 0 and column_index == max_y_dimension-1:
-                look_below = line_data[row_index+1][column_index]
-                look_left = line_data[row_index][column_index-1]
-                if look_below > element and look_left > element:
-                    risk_level += element + 1
-            elif row_index == max_x_dimension-1 and column_index == 0:
-                look_right = line_data[row_index][column_index+1]
-                look_above = line_data[row_index-1][column_index]
-                if look_above > element and look_right > element:
-                    risk_level += element + 1
-                    print("Corner Edge Case Found")
-            elif row_index == max_x_dimension-1 and column_index == max_y_dimension-1:
-                look_left = line_data[row_index][column_index-1]
-                look_above = line_data[row_index-1][column_index]
-                if look_above > element and look_left > element:
-                    risk_level += element + 1
-            else:
-                look_below = line_data[row_index+1][column_index]
-                look_above = line_data[row_index-1][column_index]
-                look_left = line_data[row_index][column_index-1]
-                look_right = line_data[row_index][column_index+1]
-                if look_above > element and look_below > element and look_left > element and look_right > element:
-                    risk_level += element + 1
-
-    print(f"Risk Level: {risk_level}")
-# -------------------------------------------------------------------------------
-
-
-
-'''
-0. read in data into matrix format
-1. create extra dimension to allow labelling
-2. pass over each element
-    2a. if adjacent elements are in basins, copy over basin id
-    2b. if no adjacent elements are in basins, use basin counter value
-    as new id, and increment basin counter
-'''
-
-
 def create_label_dim(depth_data: List[List[int]]) -> List[List[dict]]:
     '''Replace each integer depth measurement with a dictionary containing
     the depth, and an optional label value'''
@@ -137,6 +29,43 @@ def create_label_dim(depth_data: List[List[int]]) -> List[List[dict]]:
                 "label": None
             }
     return depth_data
+
+
+
+def path_finder(depth_data: List[List[int]]) -> List[List[dict]]:
+    all_points = []
+    for y in range(len(depth_data)):
+        for x in range(len(depth_data[0])):
+            # we're looking for places to explore
+            # so we need to ignore places we have been
+            # already, or cant go.
+            if depth_data[y][x] != 9:
+                all_points.append({"x":x,"y":y})
+    # all_points is now a list containing all valid points we can travel to
+
+    basins = []
+    while len(all_points) > 0:
+        # now we begin A* search!
+        start_point = all_points.pop()
+        to_explore = [start_point]
+        explored = []
+        while len(to_explore) != 0:
+            current_point = to_explore.pop()
+            explored.append(current_point)
+            # find candidates points to explore
+            # by looking for points adjacent to our current one
+            for offset in [(0,-1), (0,1), (-1,0), (1,0)]:
+                candidate = {
+                    "x":current_point["x"]+offset[0],
+                    "y":current_point["y"]+offset[1]
+                }
+                if candidate in all_points:
+                    all_points.remove(candidate)
+                    to_explore.append(candidate)
+        basins.append(explored)
+    basin_size_count = [len(basin_points) for basin_points in basins]
+    return basin_size_count
+
 
 def basin_finder(depth_data: List[List[int]]) -> List[List[dict]]:
     depth_data = create_label_dim(depth_data)
@@ -183,16 +112,16 @@ def basin_finder(depth_data: List[List[int]]) -> List[List[dict]]:
 def main():
     data = get_data(FILE_PATH)
 
-    _depth_data,basin_size_count = basin_finder(data)
+    # _depth_data,basin_size_count = basin_finder(data)
+    basin_size_count = path_finder(data)
 
-    sorted(basin_size_count,reverse=True)
+    basin_size_count = sorted(basin_size_count,reverse=True)
 
     top_three_basin_sizes = basin_size_count[0:3]
     multiplied_sizes = 1
     for size in top_three_basin_sizes:
         multiplied_sizes *= size
 
-    # 98532 - too low
     print(f"multiplied_sizes: {multiplied_sizes}")
 
 
