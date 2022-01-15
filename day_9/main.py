7bbae09162cd9472de82d5771fac988c40860d15
justletmeliv <oabwebster@gmail.com>
2022-01-15 21:13:44 +0000

    Need to Get Day 9 Part 1 Code Back

--- day_9/main.py
+++ day_9/main.py
@@ -16,18 +16,10 @@ def get_data(file_path: str):
             line_data.append(digit_data)
     # print(f"Digit Data = {digit_data}")
     # print(f"Line Data = {line_data}")
-    # print(f"Input Data = {line_data}")
     return line_data
 
-# need a number of basins counter (to track total number)
-# also need to count the number of digits within each basin
-# for digit in line
-# if digit = 9 - go to next line (but this would lose basins further to the right in the data??)
-# else current_basin_counter += 1
+def basin_labeller(line_data: List):
 
-# Ok, so what about, instead of line 25: if digit < 9, look around at surrounding measurements
-# if the surronding measurement is in a basin, and has been assigned a basin, then assign the current
-# measurement to the same basin. otherwise assign the current measurement to a new basin id?
 
 def main():
     data = get_data(FILE_PATH)
